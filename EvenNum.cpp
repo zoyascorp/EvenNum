963e8f98f99e349cc4e0415cf7c032b4ab19596d
Zoya Torosyan <javahk98@bk.ru>
2024-03-16 17:48:00 +0300

    Init

---
+++ EvenNum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main()
+{
+    int evenNumbers = 50;
+    for (evenNumbers = 0; evenNumbers <= 50; evenNumbers +=2)
+    {
+        std::cout << evenNumbers << " ";
+    }
+}
